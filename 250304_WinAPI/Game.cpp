002a10d3c62ad32cb51a93f9db28383fdabc44d4
kimmjiinn <nankimminjin@gmail.com>
2025-04-19 14:48:59 +0900

    test: random positon
    간단한 테스트

--- 250304_WinAPI/Game.cpp
+++ 250304_WinAPI/Game.cpp
@@ -16,13 +16,30 @@ Game::~Game()
 
 void Game::Init()
 {
+	srand(unsigned(time(NULL)));
+
 	KeyManager::GetInstance()->Init();
 	turnManager = new TurnManager();
 
+	// 랜덤 위치 설정-Map에서 해야함.
+
+	FPOINT arr[3];
+	for (int i = 0; i < 3; i++)
+	{
+		float x = (rand() % TILE_X);
+		float y = (rand() % TILE_Y);
+		
+		arr[i] = { x, y };
+	}
+
+	Entity* player = new Player({ (TILE_SIZE / 2) + TILE_SIZE*arr[0].x, (TILE_SIZE / 2) + TILE_SIZE * arr[0].y });
+	Entity* monster1 = new Monster({ (TILE_SIZE / 2) + TILE_SIZE * arr[1].x, (TILE_SIZE / 2) + TILE_SIZE * arr[1].y });
+	Entity* monster2 = new Monster({ (TILE_SIZE / 2) + TILE_SIZE * arr[2].x, (TILE_SIZE / 2) + TILE_SIZE * arr[2].y });
+
 	// 시작 위치 테스트용 매직넘버
-	Entity* player = new Player({ TILE_SIZE / 2, TILE_SIZE  / 2});
-	Entity* monster1 = new Monster({TILE_SIZE /2 * 5, TILE_SIZE / 2 * 5 });
-	Entity* monster2 = new Monster({TILE_SIZE /2 * 9, TILE_SIZE / 2 });
+	//Entity* player = new Player({ TILE_SIZE / 2, TILE_SIZE  / 2});
+	//Entity* monster1 = new Monster({TILE_SIZE /2 * 5, TILE_SIZE / 2 * 5 });
+	//Entity* monster2 = new Monster({TILE_SIZE /2 * 9, TILE_SIZE / 2 });
 
 	AddActor(player);
 	AddActor(monster1);
