8c92b9828a0698b2f0c88a150e6e44ca7fa1151e
alzseven <jhp7140@gmail.com>
2025-04-19 17:09:17 +0900

    fix no result on map

--- 250304_WinAPI/Map.cpp
+++ 250304_WinAPI/Map.cpp
@@ -2,8 +2,10 @@
 
 bool Map::Load(wchar_t* filePath)
 {
+    return false;
 }
 
 int Map::GetTileType(int x, int y)
 {
+    return 0;
 }
--- 250304_WinAPI/Map.h
+++ 250304_WinAPI/Map.h
@@ -4,14 +4,20 @@
 #define TILE_HEIGHT     16
 #define MAP_WIDTH       40
 #define MAP_HEIGHT      40
+#include <vector>
 
 class Map
 {
     int width;
     int height;
-    int** tiles;
+    std::vector<int> tiles;
+    // seen tile container
+    // explored tile container
+
 public:
-    Map(int width = TILE_WIDTH, int height = TILE_HEIGHT);
+    // Map(int width = TILE_WIDTH, int height = TILE_HEIGHT);
+
+    
     
     bool Load(wchar_t* filePath);
     int GetTileType(int x, int y);
