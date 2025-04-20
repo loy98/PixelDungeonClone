2c18b6883b43b00c7fcddf467b0d6fbb5f709aa4
kimmjiinn <nankimminjin@gmail.com>
2025-04-20 11:49:02 +0900

    feat: fov

--- 250304_WinAPI/FieldOfView.cpp
+++ 250304_WinAPI/FieldOfView.cpp
@@ -1,1 +1,6 @@
 ﻿#include "FieldOfView.h"
+
+void FieldOfView::Calculate(AstarScene& map, int row, float startSlope, float endSlope, ScanDirection direction)
+{
+	// 한 분면 검사
+}
--- 250304_WinAPI/FieldOfView.h
+++ 250304_WinAPI/FieldOfView.h
@@ -1,9 +1,50 @@
 ﻿#pragma once
 
+/* 검사진행 방향
+  --->				   <---
+  ㅡㅡㅡㅡㅡㅡ|ㅡㅡㅡㅡㅡㅡ
+| |\     <1>  |  <2>     \| |
+| |  \        |        \  | |
+| |    \      |      \    | |
+V |      \    |    \      | V
+  |  <8>   \  |  \   <3>  |
+  ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+  |  <7>     \|\     <4>  |
+^ |        \  |  \        | ^
+| |      \    |    \      | |
+| |    \      |      \    | |
+| |  \   <6>  |  <5>   \  |	|
+  ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+  --->				   <---
+*/
+
+struct ScanDirection
+{
+	int horizonX;
+	int verticalX;
+	int horizonY;
+	int verticalY;
+
+	ScanDirection(int hX, int vX, int hY, int vY)
+		: horizonX(hX), verticalX(vX), horizonY(hY), verticalY(vY) {}
+};
+
 class AstarScene;
 class FieldOfView
 {
 public:
-	void Calculate(AstarScene &map, )
+	ScanDirection scanDirections[8] =
+	{
+		{1, 0, 0, 1},
+		{-1, 0, 0, 1},
+		{0, 1, 1, 0},
+		{0, 1, -1, 0},
+		{-1, 0, 0, -1},
+		{1, 0, 0, -1},
+		{0, -1, 1, 0},
+		{0, -1, -1, 0}
+	};
+
+	void Calculate(AstarScene& map, int row, float startSlope, float endSlope, ScanDirection direction);
 };
 
