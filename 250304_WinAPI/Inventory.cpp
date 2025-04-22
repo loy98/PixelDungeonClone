1c1c85d022597375e175b5b759d349ad24249b9e
loy98 <yesfine13@gmail.com>
2025-04-22 18:51:02 +0900

    feat. player level...
    플레이어 레벨업, 인벤토리 구조 수정 등

--- 250304_WinAPI/Inventory.cpp
+++ 250304_WinAPI/Inventory.cpp
@@ -1,4 +1,5 @@
-#include "Inventory.h"
+﻿#include "Inventory.h"
+#include "Item.h"
 
 Inventory::Inventory()
 {
@@ -10,26 +11,50 @@ Inventory::~Inventory()
 
 void Inventory::AddItem(Item* item)
 {
-	for (int i = 0; i < items.size(); ++i)
-	{
-		if (items[i] == nullptr)
-		{
-			items[i] = item;
-			return;
-		}
-	}
+	string key = item->GetName();
+
+    // key 값에 해당하는 인덱스가 있으면 인벤토리에서
+    // 해당 인덱스 count 증가
+	auto it = indexMap.find(key);
+
+    if (it != indexMap.end()) 
+    {
+        items[it->second].count++;
+        return;
+    }
+
+    // 없으면 빈 슬롯 찾아 할당
+    for (int i = 0; i < (int)items.size(); ++i) 
+    {
+        if (items[i].count == 0) 
+        {
+            items[i].item = item;
+            items[i].count = 1;
+            indexMap[key] = i;
+            return;
+        }
+    }
 }
 
-Item* Inventory::GetItemAt(int index)
+Item* Inventory::GetItem(string name)
 {
+    if (indexMap[name] == -1) return nullptr;
+    int index = indexMap[name];
 	if (index < 0 || index >= items.size()) return nullptr;
-	return items[index];
+
+	return items[index].item;
 }
 
-Item* Inventory::RemoveItemAt(int index)
+void Inventory::UseItem(string name)
 {
-	if (index < 0 || index >= items.size()) return nullptr;
-	Item* item = items[index];
-	items[index] = nullptr;
-	return item;
+    if (indexMap[name] == -1) return;
+    int index = indexMap[name];
+	if (index < 0 || index >= items.size()) return;
+    if (items[index].count <= 0) return;
+
+    Item* item = items[index].item;
+    item->Use(owner);
+    items[index].count--;
+
+	return;
 }
--- 250304_WinAPI/Player.h
+++ 250304_WinAPI/Player.h
@@ -3,18 +3,19 @@
 
 class Level;
 class PathFinder;
+class Item;
 class Player : public Entity
 {
 public:
-
     Player(FPOINT pos, float speed, int hp, int attDmg, int defense);
-
     virtual ~Player();
 
     void Render(HDC hdc) override;
     virtual void Act(Level* level) override;
-    void Move(Level* level);
     virtual void Attack(Level* level) override;
+    void Heal(int healAmount) override;
+    
+    void Move(Level* level);
     void ActIdle(Level* level);
 
     bool NeedsInput() override { return true; }
@@ -22,5 +23,5 @@ public:
 
     void SetNextPos(FPOINT pos) { destPos = pos; }
 private:
-
+    Item* potion;
 };
